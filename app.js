7a7b46ef62f603504b58831375ba56900b122d8d
Rafaela <rafaelacss21@gmail.com>
2019-02-20 22:47:36 +0000

    remove cookie-parser to fix bug

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     passport    = require("passport"),
-    cookieParser = require("cookie-parser"),
+    //cookieParser = require("cookie-parser"),
     LocalStrategy = require("passport-local"),
     flash       = require("connect-flash"),
     Campground  = require("./models/campground"),
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
-app.use(cookieParser('secret'));
+//app.use(cookieParser('secret'));
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
