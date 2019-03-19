958c8e337efa5da7da9f926eee757a77201b6ed1
Rafaela <rafaelacss21@gmail.com>
2019-03-19 22:19:24 +0000

    css adjustments

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,15 +1,11 @@
 var counter = 0;
 function changeBG(){
     var imgs = [
-        "url(https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1444012236767-1b471c68781c?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1444012104069-996724bf4a0a?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1445888761652-fc13cbb0d819?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1445888985293-8e1b904061c4?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1444228250525-3d441b642d12?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
-        "url(https://images.unsplash.com/photo-1445308394109-4ec2920981b1?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)"
-      ]
+        "url(https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)",
+        "url(https://images.unsplash.com/photo-1510002507184-45227b3a3524?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80)",
+        "url(https://images.unsplash.com/photo-1466220549276-aef9ce186540?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)",
+        "url(https://farm1.staticflickr.com/274/18488483008_b7f6cb9196_k.jpg"
+      ];
     
     if(counter === imgs.length) counter = 0;
     $("body").css("background-image", imgs[counter]);
@@ -18,5 +14,3 @@ function changeBG(){
 }
   
   setInterval(changeBG, 4000);
-
-
